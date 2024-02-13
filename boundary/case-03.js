4e80a20588066cb6b02e2c197e2f8459e56dbeb9
chacha <86880916+hansejun@users.noreply.github.com>
2024-02-14 00:05:16 +0900

    Create case-03.js

---
+++ boundary/case-03.js
@@ -0,0 +1,12 @@
+/**
+ * first - last
+ * 포함된 양 끝을 의미한다. 0부터 10까지
+ */
+
+const people = ['Tom', 'Jerry', 'Mickey', 'Minnie'];
+
+function getStudents(first, last) {
+  // some...
+}
+
+getStudents('Tom', 'Minnie');
