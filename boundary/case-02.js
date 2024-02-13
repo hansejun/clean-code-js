2dd7b7b55924ec33c00dc3c799266981117f51eb
chacha <86880916+hansejun@users.noreply.github.com>
2024-02-14 00:05:14 +0900

    Create case-02.js

---
+++ boundary/case-02.js
@@ -0,0 +1,10 @@
+// 경계 다루기 (begin - end)
+
+// 시작은 고정되었는데 끝이 고정되어 있지 않은 경우 ex) 체크인 날짜는 고정이지만 체크아웃 날짜는 고정되어 있지 않음
+
+function reservationDate(beginDate, endDate) {
+  /// some...
+}
+
+// 흐름을 파악하기 쉬움
+reservationDate('YYYY-MM-DD', 'YYYY-MM-DD');
